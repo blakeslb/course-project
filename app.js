90fc392c40b14b696733764667241f8f1aed7e9a
Bianca Blakesley <bianca.blakesley@gmail.com>
2021-05-03 16:50:24 -0400

    creates ejs files, routes, partials

--- app.js
+++ app.js
@@ -4,27 +4,43 @@ const app = express();
 const morgan = require('morgan');
 app.use(morgan('combined'));
 
+const path = require('path');
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'ejs'); 
+
 const PORT = 3000; 
 app.listen(PORT, () =>{
     console.log('The server is listening on PORT 3000')
 });
 
 app.get('/', (req, res) => {
-    res.send ('This route points to the Home page');
+    res.render('pages/index');
 });
 
 app.get('/about', (req, res) => {
-    res.send('This route points to the About page')
+    res.render('pages/about')
 })
 
 app.get('/login', (req, res) => {
-    res.send('This route points to the Login page')
+    res.render('pages/login')
 })
 
 app.get('/admin-console', (req, res) => {
-    res.send('This route points to the Admin Console page')
+    res.render('pages/admin')
 })
 
 app.get('/admin-console/create-book', (req, res) => {
-    res.send('This route points to the Create page');
+    res.render('pages/create');
 })
+
+app.get('/books/:id', (req, res) => {
+    // let allRouteParameters = req.allRouteParameters;
+    res.render('pages/books');
+});
+
+app.get('/admin-console/update-book/:id', (req, res) => {
+    // let allRouteParameters = req.allRouteParameters;
+    res.render('pages/update');
+
+});
+
